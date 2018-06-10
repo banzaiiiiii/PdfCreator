0d15cf1aa51b250184105431ca4e2ec0649abc79
Robin Seidel <robin_sei@web.de>
2018-06-11 00:42:42 +0200

    sdfsdf

--- PdfCreator/Program.cs
+++ PdfCreator/Program.cs
@@ -47,9 +47,10 @@ namespace PdfCreator
             string text = "Ort, Datum";
             page.Canvas.DrawString(text, trueTypeFont, brush, x, y);
             PdfTextBoxField textbox = new PdfTextBoxField(page, "OrtDatum");
-            textbox.Bounds = new RectangleF(22f, 480f, 100, 22);
+            textbox.Bounds = new RectangleF(22f, 480f, 180, 22);
+            textbox.Font = trueTypeFont;
+            //textbox.Font(trueTypeFont, 8.7f);
             textbox.BorderWidth = 1f;
-            textbox.BorderStyle = PdfBorderStyle.Inset;
             pdf.Form.Fields.Add(textbox);
 
             string text2 = "Unterschrift des Besichtigers";
