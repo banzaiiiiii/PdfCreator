556dbf1c0cee77e760f3b22af3a6062fd5dff07d
rs92vegi <rs92vegi@studserv.uni-leipzig.de>
2018-06-15 09:13:21 +0200

    Erweiterungen

--- PdfCreator/Creator.cs
+++ PdfCreator/Creator.cs
@@ -1,134 +1,207 @@
 ﻿using Spire.Pdf;
+using Spire.Pdf.Fields;
 using Spire.Pdf.Graphics;
 using Spire.Pdf.Widget;
 using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace PdfCreator
 {
     class Creator
     {
+
+        PdfDocument pdf = new PdfDocument();
+
+
+        public PdfPageBase page { get; set; }
+        public PdfTrueTypeFont font { get; set; }
+        public PdfBrush brush { get; set; }
+        public PdfPen pen { get; set; }
+        public PdfMargins margin { get; set; }
+
+        float koordinateX = 10;
+        float koordianteY = 10;
+        float checkboxSizeX = 6;
+        float checkboxSizeY = 6;
+
+        int rectangleKoordinateX = 0;
+        int rectangleKoordinateY = 0;
+        int intSizeX = 538;
+        int intSizeY = 80;
+
+        int textboxHeight = 11;
+
+
+
+        // page Operationen
+
+        public void addNewPage()
+        {
+            page = pdf.Pages.Add(PdfPageSize.A4, margin);
+        }
+
+        public void setPageToEdit(int pageIndex)
+        {
+
+
+            page = pdf.Pages[pageIndex];
+        }
+
+        public void removePageFromPdf(int pageIndexToRemove)
+        {
+            pdf.Pages.RemoveAt(pageIndexToRemove);
+            pdf.Pages.RemoveAt(pageIndexToRemove);
+        }
+        // Dokumenten Operationen
+
+        public void loadPdf(string path)
+        {
+            pdf.LoadFromFile(path);
+        }
+
+        public void saveAndOpenPdf(string path)
+        {
+            pdf.SaveToFile(path);
+            pdf.Close();
+            System.Diagnostics.Process.Start(path);
+        }
+        // Layout, Styling Operationen
+
+        public void setMarginForA4Document(float valueTopBottom, float valueLeftRight)
+        {
+            PdfUnitConvertor unitCvtr = new PdfUnitConvertor();
+            margin = new PdfMargins();
+            margin.Top = unitCvtr.ConvertUnits(valueTopBottom, PdfGraphicsUnit.Centimeter, PdfGraphicsUnit.Point);
+            margin.Bottom = margin.Top;
+            margin.Left = unitCvtr.ConvertUnits(valueLeftRight, PdfGraphicsUnit.Centimeter, PdfGraphicsUnit.Point);
+            margin.Right = margin.Left;
+
+            PdfPageTemplateElement leftSpace = new PdfPageTemplateElement(margin.Left, PdfPageSize.A4.Height);
+            pdf.Template.Left = leftSpace;
+            PdfPageTemplateElement topSpace = new PdfPageTemplateElement(PdfPageSize.A4.Width, margin.Top);
+            topSpace.Foreground = true;
+            pdf.Template.Top = topSpace;
+            PdfPageTemplateElement rightSpace = new PdfPageTemplateElement(margin.Right, PdfPageSize.A4.Height);
+            pdf.Template.Right = rightSpace;
+            PdfPageTemplateElement bottomSpace = new PdfPageTemplateElement(PdfPageSize.A4.Width, margin.Bottom);
+            bottomSpace.Foreground = true;
+            pdf.Template.Bottom = bottomSpace;
+        }
+
+
+        public void setBrush(PdfBrush color)
+        {
+            brush = PdfBrushes.Black;
+        }
+
+        public void setFontToSourceSansPro(float fontSize)
+        {
+            font = new PdfTrueTypeFont(new Font("SourceSansPro", fontSize, FontStyle.Regular));
+        }
+
         
-            
+        public void setStringFormat(PdfTextAlignment alignment)
+        {
+            PdfStringFormat format = new PdfStringFormat(alignment);
 
-            PdfDocument pdf = new PdfDocument();
+        }
+
+        public void setPen(float penWidth)
+        {
+            pen = new PdfPen(Color.Black, penWidth);
+        }
+        // Abgrenzungen Einzeichnen
+
+        public void drawRectangle(int positionXChange, int positionYChange, int sizeXChange, int sizeYChange)
+        {
+            PdfGraphicsState state = page.Canvas.Save();
 
 
-            public PdfPageBase page { get; set; }
-            public PdfTrueTypeFont font { get; set; }
-            public PdfBrush brush { get; set; }
-            public PdfPen pen { get; set; }
-            public PdfMargins margin { get; set; }
+            page.Canvas.DrawRectangle(pen, new Rectangle(new Point(rectangleKoordinateX + positionXChange, rectangleKoordinateY + positionYChange), new Size(intSizeX + sizeXChange, intSizeY + sizeYChange)));
+            //page.Canvas.DrawRectangle(pen, new Rectangle(new Point(300, 450), new Size(269, 60)));
+            page.Canvas.Restore(state);
 
+        }
+        // Strings einzeichnen
 
-            // page Operationen
+        public void drawString(string text, float x, float y)
+        {
+            page.Canvas.DrawString(text, font, brush, x, y);
+        }
 
-            public void addNewPage()
+        // FelderOperationen
+
+        public void getFieldsFromPdf()
+        {
+            PdfFormWidget formWidget = pdf.Form as PdfFormWidget;
+            for (int i = 0; i < formWidget.FieldsWidget.List.Count; i++)
             {
-                page = pdf.Pages.Add(PdfPageSize.A4, margin);
+                PdfField field = formWidget.FieldsWidget.List[i] as PdfField;
+                string fieldName = field.Name;
+                Console.WriteLine(fieldName);
             }
+            Console.ReadLine();
+        }
 
-            public void setPageToEdit(int pageIndex)
+        
+        public void createCheckbox(string fieldName, float koordinateXChange, float koordianteYChange)
+        {
+            PdfCheckBoxField checkbox = new PdfCheckBoxField(page, fieldName);
+            float checkboxWidth = 10;
+            float checkboxHeight = checkboxWidth;
+            checkbox.Bounds = new RectangleF(koordinateXChange, koordianteYChange, checkboxWidth, checkboxHeight);
+            checkbox.BorderWidth = 1;
+            //checkbox.BorderStyle = PdfBorderStyle.Solid;
+            checkbox.Style = PdfCheckBoxStyle.Check;
+            pdf.Form.Fields.Add(checkbox);
+        }
+
+        public void createCheckbox2(string fieldName, float koordinateXChange, float koordianteYChange)
+        {
+            PdfCheckBoxField checkbox = new PdfCheckBoxField(page, fieldName);
+            float checkboxWidth = 50;
+            float checkboxHeight = checkboxWidth;
+            checkbox.Bounds = new RectangleF(koordinateXChange, koordianteYChange, checkboxWidth, checkboxHeight);
+            checkbox.BorderWidth = 1f;
+            //checkbox.BorderStyle = PdfBorderStyle.Solid;
+            checkbox.Style = PdfCheckBoxStyle.Circle;
+           
+            pdf.Form.Fields.Add(checkbox);
+        }
+
+        public void createTextBox(string fieldName, int koordinateXChange, int koordianteYChange, int textboxWidth)
+        {
+           
+            PdfTextBoxField textField = new PdfTextBoxField(page, fieldName);
+            textField.Bounds = new Rectangle(koordinateXChange, koordianteYChange, textboxWidth, textboxHeight);
+            textField.BorderWidth = 0f;
+            //textField.BorderStyle = PdfBorderStyle.;
+            pdf.Form.Fields.Add(textField);
+        }
+
+        public void deleteAllFieldsFromPdf()
+        {
+            PdfFormWidget formWidget = pdf.Form as PdfFormWidget;
+
+            for (int i = formWidget.FieldsWidget.List.Count - 1; i >= 0; i--)
             {
-                page = pdf.Pages[pageIndex];
-            }
-
-            public void removePageFromPdf(int pageIndexToRemove)
-            {
-                pdf.Pages.RemoveAt(pageIndexToRemove);
-                pdf.Pages.RemoveAt(pageIndexToRemove);
-            }
-            // Dokumenten Operationen
-
-            public void loadPdf(string path)
-            {
-                pdf.LoadFromFile(path);
-            }
-
-            public void saveAndOpenPdf(string path)
-            {
-                pdf.SaveToFile(path);
-                pdf.Close();
-                System.Diagnostics.Process.Start(path);
-            }
-            // Layout, Styling Operationen
-
-            public void setMarginForA4Document()
-            {
-                PdfUnitConvertor unitCvtr = new PdfUnitConvertor();
-                margin = new PdfMargins();
-                margin.Top = unitCvtr.ConvertUnits(2.54f, PdfGraphicsUnit.Centimeter, PdfGraphicsUnit.Point);
-                margin.Bottom = margin.Top;
-                margin.Left = unitCvtr.ConvertUnits(3.17f, PdfGraphicsUnit.Centimeter, PdfGraphicsUnit.Point);
-                margin.Right = margin.Left;
-
-                PdfPageTemplateElement leftSpace = new PdfPageTemplateElement(margin.Left, PdfPageSize.A4.Height);
-                pdf.Template.Left = leftSpace;
-                PdfPageTemplateElement topSpace = new PdfPageTemplateElement(PdfPageSize.A4.Width, margin.Top);
-                topSpace.Foreground = true;
-                pdf.Template.Top = topSpace;
-                PdfPageTemplateElement rightSpace = new PdfPageTemplateElement(margin.Right, PdfPageSize.A4.Height);
-                pdf.Template.Right = rightSpace;
-                PdfPageTemplateElement bottomSpace = new PdfPageTemplateElement(PdfPageSize.A4.Width, margin.Bottom);
-                bottomSpace.Foreground = true;
-                pdf.Template.Bottom = bottomSpace;
-            }
-
-
-            public void setBrush()
-            {
-                brush = PdfBrushes.Black;
-            }
-
-            public void setFontToSourceSansPro()
-            {
-                font = new PdfTrueTypeFont(new Font("SourceSansPro", 8.7f, FontStyle.Regular));
-            }
-
-            public void setStringFormat()
-            {
-                PdfStringFormat format = new PdfStringFormat(PdfTextAlignment.Right);
-
-            }
-            public void setPen(float penWidth)
-            {
-                pen = new PdfPen(Color.Black, penWidth);
-            }
-            // Abgrenzungen Einzeichnen
-
-            public void drawRectangle(int positionX, int positionY, int sizeX, int sizeY)
-            {
-                PdfGraphicsState state = page.Canvas.Save();
-
-
-                page.Canvas.DrawRectangle(pen, new Rectangle(new Point(positionX, positionY), new Size(sizeX, sizeY)));
-                //page.Canvas.DrawRectangle(pen, new Rectangle(new Point(300, 450), new Size(269, 60)));
-                page.Canvas.Restore(state);
-
-            }
-            // Strings einzeichnen
-
-            public void drawString(string text, float x, float y)
-            {
-                page.Canvas.DrawString(text, font, brush, x, y);
-            }
-
-            // FelderOperationen
-
-            public void getFieldsFromPdf()
-            {
-                PdfFormWidget formWidget = pdf.Form as PdfFormWidget;
-                for (int i = 0; i < formWidget.FieldsWidget.List.Count; i++)
-                {
-                    PdfField field = formWidget.FieldsWidget.List[i] as PdfField;
-                    string fieldName = field.Name;
-                    Console.WriteLine(fieldName);
-                }
-                Console.ReadLine();
+                PdfField field = formWidget.FieldsWidget.List[i] as PdfField;
+                formWidget.FieldsWidget.Remove(field);
             }
         }
+
+        public void drawHeader()
+        {
+            drawRectangle(0, 0, 0, 0);
+            drawString("Auftraggeber:", 2, 2);
+            createTextBox("Auftraggeber", 2 + 55, 2, 100);
+            drawString("Auftragsnummer:", 2, 20);
+            drawString("Adresse:", 2, 38);
+            drawString("Ansprechpartner:", 2, 38 + 18);
+        }
+
+        
+
+    }
 }
--- PdfCreator/Program.cs
+++ PdfCreator/Program.cs
@@ -9,40 +9,129 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// EIne PdfSeite ist ca 538 int werte breit 
+
 namespace PdfCreator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
             Creator creator = new Creator();
 
 
-            //creator.loadPdf(@"C:\Users\pc\source\repos\Hyp.Protokoll\Hyp.Api.Protokoll\Protokolle\Schwaebisch_Hall_AussenInnen.pdf");
+            //creator.loadPdf(@"D:\repos\Hyp.Protokoll\Hyp.Api.Protokoll\Protokolle\Schwaebisch_Hall_AussenInnen.pdf");
+            creator.loadPdf(@"C:\Users\robin\Desktop\Schwäbisch Hall Protokolle\Neu\Schwaebisch_Hall_AussenInnen.pdf");
 
 
-            //layout settings
-            creator.setMarginForA4Document();
-            creator.setFontToSourceSansPro();
+//layout settings
+            creator.setMarginForA4Document(0.5f,0.5f);
+            creator.setStringFormat(PdfTextAlignment.Center);
+            creator.setFontToSourceSansPro(8.7f);
             creator.setPen(0.8f);
-            creator.setBrush();
+            creator.setBrush(PdfBrushes.BlueViolet);
 
 
-            //page settings
-            creator.addNewPage();
-            //creator.setPageToEdit(1);
+//page settings
+            // creator.addNewPage();
+            creator.setPageToEdit(0);
+            //creator.removePageFromPdf(2);
 
-
+            creator.createCheckbox2("LageGrossstadtGross", 1, 1);
             // drawing stuff
-            creator.drawRectangle(18, 450, 269, 60);
-            //creator.drawString("Die Bausparkasse Schwäbisch Hall ist kacke",20, 100 );
+            //1.1
+            creator.createCheckbox("LageGrossstadtGross", 19.5f, 152);
+            creator.createCheckbox("LageGrossstadtKlein", 19.5f, 164);
+            creator.createCheckbox("LageMittelstadtGross", 19.5f, 176);
+            creator.createCheckbox("LageMittelstadtKlein", 19.5f, 188);
+            creator.createCheckbox("LageMittelstadtGross", 19.5f, 199);
+            creator.createCheckbox("LageLandgemeinde", 19.5f, 211);
+            //1.2
+            creator.createCheckbox("LageZentrum", 111, 152);
+            creator.createCheckbox("LageZentrum", 111, 164);
+            creator.createCheckbox("LageZentrum", 111, 176);
+            creator.createCheckbox("LageZentrum", 111, 188);
+            creator.createCheckbox("LageZentrum", 111, 199);
+            //1.3
+            creator.createCheckbox("LageZentrum", 202.5f, 152);
+            creator.createCheckbox("LageZentrum", 202.5f, 164);
+            creator.createCheckbox("LageZentrum", 202.5f, 176);
+            creator.createCheckbox("LageZentrum", 202.5f, 188);
+            //1.4
+            creator.createCheckbox("LageGrossstadtGross", 294, 152);
+            creator.createCheckbox("LageGrossstadtKlein", 294, 164);
+            creator.createCheckbox("LageMittelstadtGross", 294, 176);
+            creator.createCheckbox("LageMittelstadtKlein", 294, 188);
+            creator.createCheckbox("LageMittelstadtGross", 294, 199);
+            creator.createCheckbox("LageLandgemeinde", 294, 211);
+            //1.5 art
+            creator.createCheckbox("LageGrossstadtKlein", 385.5f, 164);
+            creator.createCheckbox("LageMittelstadtGross", 385.5f, 176);
+            creator.createCheckbox("LageMittelstadtKlein", 385.5f, 188);
+            creator.createCheckbox("LageMittelstadtGross", 385.5f, 199);
+            creator.createCheckbox("LageLandgemeinde", 385.5f, 211);
+            //1.5 ausbau
+            creator.createCheckbox("LageGrossstadtKlein", 476.8f, 164);
+            creator.createCheckbox("LageMittelstadtGross", 476.8f, 176);
+            creator.createCheckbox("LageMittelstadtKlein", 476.8f, 188);
+            creator.createCheckbox("LageMittelstadtGross", 476.8f, 199);
+            //1.6
+            creator.createCheckbox("LageZentrum", 19.5f, 234);
+            creator.createCheckbox("LageZentrum", 19.5f, 246);
+            creator.createCheckbox("LageZentrum", 19.5f, 258);
+            creator.createCheckbox("LageZentrum", 19.5f, 270);
+            creator.createCheckbox("LageZentrum", 19.5f, 281);
+
+            creator.createCheckbox("LageZentrum", 111, 234);
+            creator.createCheckbox("LageZentrum", 111, 246);
+            creator.createCheckbox("LageZentrum", 111, 258);
+            creator.createCheckbox("LageZentrum", 111, 270);
+            creator.createCheckbox("LageZentrum", 111, 281);
+
+            creator.createCheckbox("LageZentrum", 202.5f, 234);
+            creator.createCheckbox("LageZentrum", 202.5f, 246);
+            creator.createCheckbox("LageZentrum", 202.5f, 258);
+            creator.createCheckbox("LageZentrum", 202.5f, 270);
+            creator.createCheckbox("LageZentrum", 202.5f, 281);
+
+            creator.createCheckbox("LageZentrum", 294, 234);
+            creator.createCheckbox("LageZentrum", 294, 246);
+            creator.createCheckbox("LageZentrum", 294, 258);
+            creator.createCheckbox("LageZentrum", 294, 270);
+
+            creator.createCheckbox("LageZentrum", 385.5f, 234);
+            creator.createCheckbox("LageZentrum", 385.5f, 246);
+            creator.createCheckbox("LageZentrum", 385.5f, 258);
+            creator.createCheckbox("LageZentrum", 385.5f, 270);
+            creator.createCheckbox("LageZentrum", 385.5f, 281);
+
+            //1.7
+            creator.createCheckbox("LageZentrum", 19.5f, 305);
+            creator.createCheckbox("LageZentrum", 19.5f, 317);
+            creator.createCheckbox("LageZentrum", 19.5f, 328.5f);
+
+            creator.createCheckbox("LageZentrum", 111, 305);
+            creator.createCheckbox("LageZentrum", 111, 317);
+            creator.createCheckbox("LageZentrum", 111, 328.5f);
+
+            creator.createCheckbox("LageZentrum", 202.5f, 305);
+            creator.createCheckbox("LageZentrum", 202.5f, 317);
+            creator.createCheckbox("LageZentrum", 202.5f, 328.5f);
+
+            creator.createCheckbox("LageZentrum", 294, 305);
+            creator.createCheckbox("LageZentrum", 294, 317);
+            creator.createCheckbox("LageZentrum", 294, 328.5f);
+
+
+
 
             // felder Operationen
 
             //creator.getFieldsFromPdf();
 
-
-            creator.saveAndOpenPdf(@"C:/users/pc/desktop/SpirePdfCreation/testPdf.pdf");
+            //creator.deleteAllFieldsFromPdf();
+            creator.saveAndOpenPdf(@"C:/users/robin/desktop/tesdtff1ffffdffdffff3.pdf");
 
 
         }
